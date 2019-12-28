47b2a8195b4f7b77aef57b3f29b6bbbfd0f7c7e4
shuax <admin@shuax.com>
2019-12-28 14:46:59 +0800

    优化多语言

--- src/view/demo.vue
+++ src/view/demo.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <p style="padding-bottom: 10px;">{{$t('demo_tips')}}</p>
     <Row :gutter="10">
       <Col :sm="24" :md="12" :lg="12" :xl="8" :xxl="6" v-for="(info, i) in action_list" :key="`info-${i}`" style="padding-bottom: 10px;">
         <Card shadow>
@@ -7,7 +8,7 @@
                 {{info[0]}}
             </p>
             <a href="#" slot="extra" @click.prevent="copy" class="copyCode" data-clipboard-action="copy" :data-clipboard-text="stringify(info[1])">
-                复制
+                {{$t('copy_btn')}}
             </a>
             <codemirror :value="JSON.stringify(info[1])" :options="option">
             </codemirror>
@@ -45,16 +46,18 @@ export default {
   data () {
     return {
       action_list: [
-        ['锁屏', [['Execute', 'rundll32 user32.dll,LockWorkStation']]],
         ['延迟1秒', [['Internal', 'Delay', '1000']]],
+        ['锁屏', [['Execute', 'rundll32 user32.dll,LockWorkStation']]],
+        ['关于电脑', [['Execute', 'rundll32 shell32.DLL,ShellAbout']]],
+        ['控制面板', [['Execute', 'rundll32 shell32.dll,Control_RunDLL']]],
         ['截图参考同时复制', [['Screenshot', 'ToClipboard'], ['Screenshot', 'Reference']]],
         ['设备管理器', [['Execute', 'rundll32 devmgr.dll DeviceManager_Execute']]],
+        ['Everything搜索选中内容', [['SendKeys', 'Ctrl+C'], ['Execute', 'Everything.exe -s %clipboard%']]],
+        ['Python运行选中代码', [['SendKeys', 'Ctrl+C'], ['Execute', 'python -i -c %clipboard%']]],
         ['打开QQ', [['Execute', '"C:\\Program Files (x86)\\Tencent\\QQ\\Bin\\QQ.exe"']]],
         ['打开微信', [['Execute', '"C:\\Program Files (x86)\\Tencent\\WeChat\\WeChat.exe"']]],
         ['打开TIM', [['Execute', '"C:\\Program Files (x86)\\Tencent\\TIM\\Bin\\TIM.exe"']]],
-        ['系统属性', [['Execute', 'rundll32 shell32.dll,Control_RunDLL sysdm.cpl,,1']]],
-        ['控制面板', [['Execute', 'rundll32 shell32.dll,Control_RunDLL']]],
-        ['关于电脑', [['Execute', 'rundll32 shell32.DLL,ShellAbout']]]
+        ['系统属性', [['Execute', 'rundll32 shell32.dll,Control_RunDLL sysdm.cpl,,1']]]
       ],
       option: {
         lineNumbers: false,
