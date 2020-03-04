8fa1769dcace3440bcd9355f0113ea993fb6386c
shuax <admin@shuax.com>
2020-03-04 23:23:20 +0800

    自动兼容配置

--- src/store/module/settings.js
+++ src/store/module/settings.js
@@ -11,6 +11,40 @@ export default {
   },
   mutations: {
     setSettings (state, settings) {
+      // 版本兼容
+      if (!settings.cfg.HotCorner) {
+        settings.cfg.HotCorner = {
+          'BottomLeft': {
+            'Actions': [
+              ['SendKeys', 'Win']
+            ],
+            'Name': '开始菜单',
+            'Valid': true
+          },
+          'BottomRight': {
+            'Actions': [
+              ['SendKeys', 'Win+D']
+            ],
+            'Name': '显示桌面',
+            'Valid': true
+          },
+          'Open': false,
+          'TopLeft': {
+            'Actions': [
+              ['SendKeys', 'Win+Tab']
+            ],
+            'Name': '切换桌面',
+            'Valid': true
+          },
+          'TopRight': {
+            'Actions': [
+              ['SendKeys', 'Alt+F4']
+            ],
+            'Name': '关闭程序',
+            'Valid': false
+          }
+        }
+      }
       state.cfg = settings.cfg
       state.gestures = settings.gestures
       state.gestures['WheelSwitchUp'] = state.gestures['placeholder']
